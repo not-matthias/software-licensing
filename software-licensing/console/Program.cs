b9014dfa9613128fd88d762b29f9a33a08334829
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-11-13 12:07:55 +0100

    Added the client boilerplate

--- software-licensing/console/Program.cs
+++ software-licensing/console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
 using utils;
@@ -12,7 +13,10 @@ namespace console
 
         static async Task Main(string[] args)
         {
-            await GetPublicKey();
+            var publicKey = await GetPublicKey();
+            var program = await GetProgram(publicKey, "AAAA-BBBB-CCCC-DDDD");
+
+            LaunchProgram(Assembly.Load(program));
         }
 
         public static T Deserialize<T>(string jsonContent)
@@ -23,19 +27,29 @@ namespace console
             });
         }
 
-        public static async Task GetPublicKey()
+        public static async Task<RSAParametersSerializable> GetPublicKey()
         {
-            try
-            {
-                var response = await client.GetStringAsync("/license/public_key");
-                var packet = Deserialize<Packet<RSAParametersSerializable>>(response);
+            var response = await client.GetStringAsync("/license/public_key");
+            var packet = Deserialize<Packet<RSAParametersSerializable>>(response);
 
-                Console.WriteLine(packet.IsValid());
-            }
-            catch (Exception exception)
+            if (!packet.IsValid())
             {
-                Console.WriteLine(exception.ToString());
+                Console.WriteLine("Packet is not valid.");
+                throw new InvalidOperationException();
             }
+
+            return packet.Data;
+        }
+
+        public static async Task<byte[]> GetProgram(RSAParametersSerializable publicKey, string licenseKey)
+        {
+            // Get the program from the server
+            return new byte[100];
+        }
+
+        public static async void LaunchProgram(Assembly assembly)
+        {
+            // TODO: Launch it with the IProgramLoader.cs
         }
     }
 }
