7342c563c52bfa7cf017cae822a1a55004d35e96
Lukas Rauchenzauner <rauchi13.lr@gmail.com>
2019-11-13 11:05:47 +0100

    Initial commit

--- software-licensing/console/Program.cs
+++ software-licensing/console/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using utils;
 
 namespace console
 {
     class Program
     {
-        static void Main(string[] args)
+        public static HttpClient client = new HttpClient() { BaseAddress = new Uri("http://localhost:53696") };
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _ = await GetPublicKey();
+        }
+
+        public static async Task<string> GetPublicKey()
+        {
+            // DecryptedPacket<string>try 
+            try
+            {
+                var response = await client.GetStringAsync("/license/public_key");
+
+                var packet = JsonSerializer.Deserialize<DecryptedPacket<RSAParametersSerializable>>(response);
+
+
+                Console.WriteLine("Testasdf");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+            Console.WriteLine("Hey!");
+
+            return "You suck";
         }
     }
 }
