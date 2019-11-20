656f78abb763ffa0be6a03ae6a68a9b97d959be6
DeaGiXe <if140049@htblaperg.onmicrosoft.com>
2019-11-20 20:45:53 +0100

    encrypt, decrypt  test

--- software-licensing/sample_software/Intializer.cs
+++ software-licensing/sample_software/Intializer.cs
@@ -10,13 +10,19 @@ namespace sample_software
 
         public static void Init()
         {
+            Console.WriteLine("Enter your data to encrypt:");
             var userInput = Console.ReadLine();
 
             var byteData = userInput.ToByteArray();
 
             var (publicKey, privateKey) = cryptoManager.GenerateKeyPair();
             var encrypt = cryptoManager.Encrypt(publicKey, byteData);
+            Console.WriteLine("Your encrypted data:");
             Console.WriteLine(encrypt.EncryptedData.FromByteArray());
+
+            var decrypt = cryptoManager.Decrypt(privateKey, encrypt);
+            // Console.WriteLine("and decrypted data:");
+            // Console.WriteLine(decrypt.ToString());
         }
     }
 }
