9175c07b1dcaef1b598567f205a660bf3a699a8b
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-11-21 08:20:34 +0100

    Updated the styling

--- software-licensing/sample_software/Intializer.cs
+++ software-licensing/sample_software/Intializer.cs
@@ -12,17 +12,31 @@ namespace sample_software
         {
             Console.WriteLine("Enter your data to encrypt:");
             var userInput = Console.ReadLine();
+            Console.WriteLine();
 
             var byteData = userInput.ToByteArray();
-
             var (publicKey, privateKey) = cryptoManager.GenerateKeyPair();
+
+            //
+            // Encrypt
+            //
             var encrypt = cryptoManager.Encrypt(publicKey, byteData);
             Console.WriteLine("Your encrypted data:");
-            Console.WriteLine(encrypt.EncryptedData.FromByteArray());
+            Console.WriteLine(encrypt.EncryptedData.FromByteArray() + "\n");
 
+            //
+            // Decrypt
+            //
             var decrypt = cryptoManager.Decrypt(privateKey, encrypt);
-            // Console.WriteLine("and decrypted data:");
-            // Console.WriteLine(decrypt.ToString());
+            Console.WriteLine("Your decrypted data:");
+            Console.WriteLine(decrypt.FromByteArray() + "\n");
+
+            //
+            // Hash
+            //
+            var dataHash = cryptoManager.GenerateHash(decrypt);
+            Console.WriteLine("Your data hash:");
+            Console.WriteLine(dataHash);
         }
     }
 }
